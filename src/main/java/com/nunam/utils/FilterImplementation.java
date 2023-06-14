0a84cfe399492d52e69f79e6d3d05b01c4ae0c3d
sakthivel-nunam <sakthivel@nunam.com>
2023-06-14 16:13:26 +0530

    multiple order by support added to impl

--- src/main/java/com/nunam/utils/FilterImplementation.java
+++ src/main/java/com/nunam/utils/FilterImplementation.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import javax.lang.model.util.ElementScanner6;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Order;
 import javax.persistence.criteria.Path;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
@@ -18,138 +18,124 @@ public class FilterImplementation<T> {
 	@PersistenceContext
 	private EntityManager entityManager;
 
-	public List<T> filter(Map<String, Object> filter, String orderby, Integer offset, Integer limit, T to) {
-		
+						  
+	public List<T> filter(Map<String, Object> filter, Integer offset, Integer limit, List<String> orderby, T to ) {
+
 		CriteriaBuilder cb = entityManager.getCriteriaBuilder();
-        CriteriaQuery<T> query = (CriteriaQuery<T>) cb.createQuery(to.getClass());
-        Root<T> t = (Root<T>) query.from(to.getClass());
-        List<Predicate> predicates = new ArrayList();
-        Predicate finalPredicate = null;
-        if(filter.containsKey("and"))
-        {
-            Map<String, Object> andCriteria = (Map<String, Object>) filter.get("and");
-            for (Map.Entry<String, Object> criterion : andCriteria.entrySet()) {
-                String key = criterion.getKey();
-                Path<String> attributePath = t.get(key);
-                Predicate predicate = cb.equal(attributePath, criterion.getValue());
-                predicates.add(predicate);
-            }
-            finalPredicate = cb.and(predicates.toArray(new Predicate[predicates.size()]));
-        }
-        else if(filter.containsKey("or"))
-        {
-            Map<String, Object> orCriteria = (Map<String, Object>) filter.get("or");
-            for (Map.Entry<String, Object> criterion : orCriteria.entrySet()) {
-                String key = criterion.getKey();
-                Path<String> attributePath = t.get(key);
-                Predicate predicate = cb.equal(attributePath, criterion.getValue());
-                predicates.add(predicate);
-            }
-            finalPredicate = cb.or(predicates.toArray(new Predicate[predicates.size()]));
-        }
-        else
-        {
-            for(String key: filter.keySet()) {
-                Path<String> party = t.get(key);
-                predicates.add(cb.equal(party,filter.get(key)));
-            }
-            finalPredicate = cb.and(predicates.toArray(new Predicate[predicates.size()]));
-        }
-       
-        query.select(t)
-            .where(finalPredicate);
-        
-        if (orderby != null && !orderby.isEmpty()) {
-            Path<?> orderPath = t.get(orderby);
-            query.orderBy(cb.desc(orderPath));
-        }
-        return entityManager.createQuery(query)
-            .setFirstResult(offset).setMaxResults(limit).getResultList();
+		CriteriaQuery<T> query = (CriteriaQuery<T>) cb.createQuery(to.getClass());
+		Root<T> t = (Root<T>) query.from(to.getClass());
+		List<Predicate> predicates = new ArrayList();
+		Predicate finalPredicate = null;
+		if (filter.containsKey("and")) {
+			Map<String, Object> andCriteria = (Map<String, Object>) filter.get("and");
+			for (Map.Entry<String, Object> criterion : andCriteria.entrySet()) {
+				String key = criterion.getKey();
+				Path<String> attributePath = t.get(key);
+				Predicate predicate = cb.equal(attributePath, criterion.getValue());
+				predicates.add(predicate);
+			}
+			finalPredicate = cb.and(predicates.toArray(new Predicate[predicates.size()]));
+		} else if (filter.containsKey("or")) {
+			Map<String, Object> orCriteria = (Map<String, Object>) filter.get("or");
+			for (Map.Entry<String, Object> criterion : orCriteria.entrySet()) {
+				String key = criterion.getKey();
+				Path<String> attributePath = t.get(key);
+				Predicate predicate = cb.equal(attributePath, criterion.getValue());
+				predicates.add(predicate);
+			}
+			finalPredicate = cb.or(predicates.toArray(new Predicate[predicates.size()]));
+		} else {
+			for (String key : filter.keySet()) {
+				Path<String> party = t.get(key);
+				predicates.add(cb.equal(party, filter.get(key)));
+			}
+			finalPredicate = cb.and(predicates.toArray(new Predicate[predicates.size()]));
+		}
+
+		query.select(t).where(finalPredicate);
+		List<Order> orderByPaths = new ArrayList<>();
+		if (orderby != null && !orderby.isEmpty()) {
+			for (String orderbyFiled : orderby) {
+				Path<?> orderPath = t.get(orderbyFiled);
+				orderByPaths.add(cb.desc(orderPath));
+			}
+			query.orderBy(orderByPaths);
+
+		}
+		return entityManager.createQuery(query).setFirstResult(offset).setMaxResults(limit).getResultList();
 	}
 
 	public Long filterCount(Map<String, Object> filter, T to) {
-		
+
 		CriteriaBuilder cb = entityManager.getCriteriaBuilder();
 		CriteriaQuery<Long> cq = cb.createQuery(Long.class);
-        Root<T> t = (Root<T>) cq.from(to.getClass());
-        List<Predicate> predicates = new ArrayList();
-        Predicate finalPredicate = null;
-        if(filter.containsKey("and"))
-        {
-            Map<String, Object> andCriteria = (Map<String, Object>) filter.get("and");
-            for (Map.Entry<String, Object> criterion : andCriteria.entrySet()) {
-                String key = criterion.getKey();
-                Path<String> attributePath = t.get(key);
-                Predicate predicate = cb.equal(attributePath, criterion.getValue());
-                predicates.add(predicate);
-            }
-            finalPredicate = cb.and(predicates.toArray(new Predicate[predicates.size()]));
-        }
-        else if(filter.containsKey("or"))
-        {
-            Map<String, Object> orCriteria = (Map<String, Object>) filter.get("or");
-            for (Map.Entry<String, Object> criterion : orCriteria.entrySet()) {
-                String key = criterion.getKey();
-                Path<String> attributePath = t.get(key);
-                Predicate predicate = cb.equal(attributePath, criterion.getValue());
-                predicates.add(predicate);
-            }
-            finalPredicate = cb.or(predicates.toArray(new Predicate[predicates.size()]));
-        }
-        else
-        {
-            for(String key: filter.keySet()) {
-                Path<String> party = t.get(key);
-                predicates.add(cb.equal(party,filter.get(key)));
-            }
-            finalPredicate = cb.and(predicates.toArray(new Predicate[predicates.size()]));
-        }
-        
-        
-        cq.select(cb.count(t));
-        cq.where(finalPredicate);
-        return entityManager.createQuery(cq).getSingleResult();
+		Root<T> t = (Root<T>) cq.from(to.getClass());
+		List<Predicate> predicates = new ArrayList();
+		Predicate finalPredicate = null;
+		if (filter.containsKey("and")) {
+			Map<String, Object> andCriteria = (Map<String, Object>) filter.get("and");
+			for (Map.Entry<String, Object> criterion : andCriteria.entrySet()) {
+				String key = criterion.getKey();
+				Path<String> attributePath = t.get(key);
+				Predicate predicate = cb.equal(attributePath, criterion.getValue());
+				predicates.add(predicate);
+			}
+			finalPredicate = cb.and(predicates.toArray(new Predicate[predicates.size()]));
+		} else if (filter.containsKey("or")) {
+			Map<String, Object> orCriteria = (Map<String, Object>) filter.get("or");
+			for (Map.Entry<String, Object> criterion : orCriteria.entrySet()) {
+				String key = criterion.getKey();
+				Path<String> attributePath = t.get(key);
+				Predicate predicate = cb.equal(attributePath, criterion.getValue());
+				predicates.add(predicate);
+			}
+			finalPredicate = cb.or(predicates.toArray(new Predicate[predicates.size()]));
+		} else {
+			for (String key : filter.keySet()) {
+				Path<String> party = t.get(key);
+				predicates.add(cb.equal(party, filter.get(key)));
+			}
+			finalPredicate = cb.and(predicates.toArray(new Predicate[predicates.size()]));
+		}
+
+		cq.select(cb.count(t));
+		cq.where(finalPredicate);
+		return entityManager.createQuery(cq).getSingleResult();
 	}
-	
+
 	public List<T> filter(Map<String, Object> filter, T to) {
 		CriteriaBuilder cb = entityManager.getCriteriaBuilder();
-        CriteriaQuery<T> query = (CriteriaQuery<T>) cb.createQuery(to.getClass());
-        Root<T> t = (Root<T>) query.from(to.getClass());
-        List<Predicate> predicates = new ArrayList();
-        Predicate finalPredicate = null;
-        if(filter.containsKey("and"))
-        {
-            Map<String, Object> andCriteria = (Map<String, Object>) filter.get("and");
-            for (Map.Entry<String, Object> criterion : andCriteria.entrySet()) {
-                String key = criterion.getKey();
-                Path<String> attributePath = t.get(key);
-                Predicate predicate = cb.equal(attributePath, criterion.getValue());
-                predicates.add(predicate);
-            }
-            finalPredicate = cb.and(predicates.toArray(new Predicate[predicates.size()]));
-        }
-        else if(filter.containsKey("or"))
-        {
-            Map<String, Object> orCriteria = (Map<String, Object>) filter.get("or");
-            for (Map.Entry<String, Object> criterion : orCriteria.entrySet()) {
-                String key = criterion.getKey();
-                Path<String> attributePath = t.get(key);
-                Predicate predicate = cb.equal(attributePath, criterion.getValue());
-                predicates.add(predicate);
-            }
-            finalPredicate = cb.or(predicates.toArray(new Predicate[predicates.size()]));
-        }
-        else
-        {
-            for(String key: filter.keySet()) {
-                Path<String> party = t.get(key);
-                predicates.add(cb.equal(party,filter.get(key)));
-            }
-            finalPredicate = cb.and(predicates.toArray(new Predicate[predicates.size()]));
-        }
-        
-        query.select(t)
-        .where(finalPredicate);
-        return entityManager.createQuery(query).getResultList();
+		CriteriaQuery<T> query = (CriteriaQuery<T>) cb.createQuery(to.getClass());
+		Root<T> t = (Root<T>) query.from(to.getClass());
+		List<Predicate> predicates = new ArrayList();
+		Predicate finalPredicate = null;
+		if (filter.containsKey("and")) {
+			Map<String, Object> andCriteria = (Map<String, Object>) filter.get("and");
+			for (Map.Entry<String, Object> criterion : andCriteria.entrySet()) {
+				String key = criterion.getKey();
+				Path<String> attributePath = t.get(key);
+				Predicate predicate = cb.equal(attributePath, criterion.getValue());
+				predicates.add(predicate);
+			}
+			finalPredicate = cb.and(predicates.toArray(new Predicate[predicates.size()]));
+		} else if (filter.containsKey("or")) {
+			Map<String, Object> orCriteria = (Map<String, Object>) filter.get("or");
+			for (Map.Entry<String, Object> criterion : orCriteria.entrySet()) {
+				String key = criterion.getKey();
+				Path<String> attributePath = t.get(key);
+				Predicate predicate = cb.equal(attributePath, criterion.getValue());
+				predicates.add(predicate);
+			}
+			finalPredicate = cb.or(predicates.toArray(new Predicate[predicates.size()]));
+		} else {
+			for (String key : filter.keySet()) {
+				Path<String> party = t.get(key);
+				predicates.add(cb.equal(party, filter.get(key)));
+			}
+			finalPredicate = cb.and(predicates.toArray(new Predicate[predicates.size()]));
+		}
+
+		query.select(t).where(finalPredicate);
+		return entityManager.createQuery(query).getResultList();
 	}
 }
